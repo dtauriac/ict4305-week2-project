bae7b43721ff4d4ee18ddcf1f3778376d0148f81
eyevanovich <54329731+eyevanovich@users.noreply.github.com>
2020-06-24 14:57:06 -0700

    Julian Class setup and Test File

---
+++ JulianNumberProject/src/main/java/com/ict4305group3/juliannumberproject/JulianConverter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ict4305group3.juliannumberproject;
+
+/**
+ *
+ * @author ICT4305 Group 3
+ */
+public class JulianConverter {
+
+    /**
+     *
+     * @param day
+     * @param month
+     * @param year
+     * @return Julian date
+     */
+    public static int toJulianNumber(int day, int month, int year){
+
+        int JulianNumber = (1461 * (year + 4800 + (month - 14) / 12)) / 4
+                + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12
+                - (3 * ((year + 4900 + (month - 14) / 12) / 100)) 
+                / 4 + day - 32075;
+        return JulianNumber;
+    }
+
+    /**
+     *
+     * @param JulianNumber
+     * @return Gregorian Date
+     */
+    public static int[] fromJulianNumber(int JulianNumber) {
+
+        int l = JulianNumber + 68569;
+        int n = (4 * l) / 146097;
+        l = l - (146097 * n + 3) / 4;
+        int i = (4000 * (l + 1)) / 1461001;
+        l = l - (1461 * i) / 4 + 31;
+        int j = (80 * l) / 2447;
+        int day = l - (2447 * j) / 80;
+        l = j / 11;
+        int month = j + 2 - (12 * l);
+        int year = 100 * (n - 49) + i + l;
+
+        int[] gregorian = new int[3];
+        day = gregorian[0];
+        month = gregorian[1];
+        year = gregorian[2];
+
+        return gregorian;
+    }
+
+}
---
+++ JulianNumberProject/src/test/java/com/ict4305group3/juliannumberproject/JulianConverterTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ict4305group3.juliannumberproject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//    public static void main(final String[] args) {
+//
+//        int[] gregorian = fromJulianNumber(1739327);
+//        System.out.println(toJulianNumber(1, 2, 1957));
+//        System.out.println(gregorian[1] + "/" + gregorian[0] + "/" + gregorian[2]);
+//        
+//    }
+
+/**
+ *
+ * @author ICT4305 Group 3
+ */
+public class JulianConverterTest {
+   
+    /**
+     * Test of toJulianNumber method, of class JulianConverter.
+     */
+    @Test
+    public void testToJulianNumber() {
+        //put test here
+    }
+
+    /**
+     * Test of fromJulianNumber method, of class JulianConverter.
+     */
+    @Test
+    public void testFromJulianNumber() {
+        //put test here
+    }
+    
+}
