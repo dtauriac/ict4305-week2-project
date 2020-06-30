c08adfaf2f19ca44f1f4123eb1a708fc696f612f
eyevanovich <54329731+eyevanovich@users.noreply.github.com>
2020-06-30 10:42:53 -0700

    Adding template for Date class

---
+++ JulianNumberProject/src/main/java/com/ict4305group3/juliannumberproject/Date.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ict4305group3.juliannumberproject;
+
+/**
+ *
+ * @author ipiesh
+ */
+public class Date {
+    
+    private int myJulianNumber;
+    
+    public Date(){
+    
+    }; // If no arguments were provided then default date to epoch time, which January 1st, 1970. 
+
+    public Date( Date date){
+    
+    };  
+
+    public Date( int day, int month, int year){
+    
+    };  
+
+    public int getDay(){
+    
+    }; 
+
+    public int getMonth(){
+    
+    }; 
+
+    public int getYear(){
+    
+    }; 
+
+    public static boolean isLeapYear( int year ){
+    
+    }; 
+
+    public static int getLastDayOfMonth( int month, int year ){
+    
+    }; 
+
+    private int toJulianNumber(int day, int month, int year){
+    
+    }; 
+
+    private int[] fromJulianNumber(){
+        
+    }; 
+
+}
